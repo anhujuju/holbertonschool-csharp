dd569a5e98894495cc079a54e6a65e88aa57ca2a
anhujuju <anhujuju1@gmail.com>
2021-05-03 21:51:35 -0500

    inheritance

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>Defines a shape</summary>
+class Shape
+{
+    /// <summary>Virtual method that Calculates a area</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
