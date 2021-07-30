56467fdf32c6642a5bb7787202e6d1af5fbfc8de
anhujuju <anhujuju1@gmail.com>
2021-07-30 16:11:42 -0500

    interfaces

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+///<summary>Base class</summary>
+public abstract class Base
+{
+    ///<summary>Property name</summary>
+    private string name {get; set;}
+    ///<summary>String representation of the object</summary>
+    public override string ToString()
+    {
+        return $"{this.name} is a {this.GetType().Name}";
+    }
+}
+
+
+///<summary>IInteractive interface</summary>
+public interface IInteractive
+{
+    ///<summary>Interact method</summary>
+    void Interact();
+}
+///<summary>IBreakable interface</summary>
+public interface IBreakable
+{
+    ///<summary>durability property</summary>
+    int durability {get; set;}
+    ///<summary>Break method</summary>
+    void Break();
+}
+///<summary>ICollectable interface</summary>
+public interface ICollectable
+{
+    ///<summary>isCollected property</summary>
+    bool isCollected {get; set;}
+    ///<summary>Collect method</summary>
+    void Collect();
+}
+///<summary>TestObject Class</summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    ///<summary>durability property</summary>
+    public int durability {get; set;}
+    ///<summary>isCollected property</summary>
+    public bool isCollected {get; set;}
+    ///<summary>name property</summary>
+    public string name {get; set;}
+    ///<summary> Interact Method </summary>
+    public void Interact() {} 
+    ///<summary>Break Method</summary>
+    public void Break() {}   
+    ///<summary>Collect Method</summary>
+    public void Collect() {}
+}
---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+///<summary>Base Class</summary>
+abstract class Base
+{
+    ///<summary>Property name</summary>
+    public string name { set; get; }
+
+    ///<summary>Returns the string representation of a Base object</summary>
+    public override string ToString()
+    {
+        return $"{this.name} is a {this.GetType().Name}";
+    }
+}
+
+///<summary>IInteractive Interface</summary>
+interface IInteractive
+{
+    ///<summary>Delcaration of method Interact</summary>
+
+    void Interact();
+}
+
+///<summary>IBreakable Interface</summary>
+interface IBreakable
+{
+    ///<summary>Delcaration of property durability</summary>
+    int durability { set; get; }
+
+    ///<summary>Delcaration of method Break</summary>
+    void Break();
+}
+
+///<summary>ICollectable Interface</summary>
+interface ICollectable
+{
+    ///<summary>Delcaration of property isCollected</summary>
+    bool isCollected { set; get; }
+
+    ///<summary>Delcaration of method Collect</summary>
+    void Collect();
+}
+
+
+
+///<summary>Door Class</summary>
+class Door : Base, IInteractive
+{
+    ///<summary>Base Constructor</summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+    ///<summary>Method Interact</summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {this.name}. It's locked.");
+    }
+}
